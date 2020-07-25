a919c14fa67ee793085eec54ff5d3cfb76492b50
Jakob Buis <jakob@jakobbuis.nl>
2020-07-25 19:51:39 +0200

    cannot create empty notes

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -3,7 +3,9 @@
         <textarea v-model="note" class="flex-1 h-16 p-2"></textarea>
         <button
             @click.prevent="store"
-            class="bg-gray-700 text-white font-bold py-2 px-4 ml-2">
+            class="text-white font-bold py-2 px-4 ml-2"
+            :class="{ 'bg-gray-700': note, 'bg-gray-500': !note}"
+            :disabled="!note">
             Store
         </button>
     </form>
