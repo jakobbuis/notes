6df943816c226ac68e73ead345cbdb1ccfa35e51
Jakob Buis <jakob@jakobbuis.nl>
2020-07-25 20:07:55 +0200

    render creation date of note

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -3,6 +3,9 @@
         <div class="px-4 py-2">
             <p class="text-gray-700 text-base" v-html="renderedText" />
         </div>
+        <div class="px-4 py-4">
+            <span class="text-gray-600 text-sm" v-html="created_at" />
+        </div>
     </div>
 </template>
 
@@ -13,6 +16,11 @@ export default {
     computed: {
         renderedText() {
             return this.note.text.replace(/(?:\r\n|\r|\n)/g, '<br>');
+        },
+
+        created_at() {
+            const date = new Date(this.note.created_at);
+            return `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
         },
     },
 };
