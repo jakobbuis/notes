14dc805eddc0edfcd6246fcb793ae5a9ad1bf03b
Jakob Buis <jakob@jakobbuis.nl>
2020-07-25 20:53:37 +0200

    enable sourcemaps

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,7 +3,7 @@ let mix = require('laravel-mix');
 mix.js('src/app.js', 'dist/');
 mix.postCss('src/app.css', 'dist/', [
     require('tailwindcss'),
-]);
+]).sourceMaps();
 
 mix.copy('src/index.html', 'dist/');
 
