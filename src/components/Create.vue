fa5ee1ec53aea8bfe9ba3a3cbe501f1dd00445ac
Jakob Buis <jakob@jakobbuis.nl>
2020-07-26 15:24:42 +0200

    make header and Create fixed action bars with backgrounds

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,14 +1,16 @@
 <template>
-    <form class="flex sticky bottom-0 pt-4 bg-gray-300">
-        <Record @voice="(text) => this.note += text"></Record>
-        <textarea v-model="note" class="flex-1 h-16 p-2 italic"></textarea>
-        <button
-            @click.prevent="store"
-            class="text-white font-bold py-2 px-4 ml-2"
-            :class="{ 'bg-gray-700': note, 'bg-gray-500': !note}"
-            :disabled="!note">
-            Store
-        </button>
+    <form class="fixed bottom-0 inset-x-0 p-4 bg-gray-700">
+        <div class="flex justify-between max-w-lg">
+            <Record @voice="(text) => this.note += text"></Record>
+            <textarea v-model="note" class="flex-1 h-16 p-2 italic"></textarea>
+            <button
+                @click.prevent="store"
+                class="text-white font-bold py-2 px-4 ml-2"
+                :class="{ 'bg-gray-700': note, 'bg-gray-500': !note}"
+                :disabled="!note">
+                Store
+            </button>
+        </div>
     </form>
 </template>
 
