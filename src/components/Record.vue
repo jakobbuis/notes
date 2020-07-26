619565a8ee23b4e059dd71f39ca6604ee3ca084f
Jakob Buis <jakob.buis@in10.nl>
2020-07-26 13:37:08 +0200

    show a pop-up with explanation when voice recognition is disabled

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -3,7 +3,6 @@
     @click.prevent
     @mousedown.prevent="startRecording"
     @mouseup.prevent="stopRecording"
-    :disabled="!recordingEnabled"
     class="w-16 text-white p-4 mr-2"
     :class="recordingStateColour">
     <svg fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clip-rule="evenodd"></path></svg>
@@ -58,6 +57,10 @@ export default {
         },
 
         startRecording() {
+            if (!this.recordingEnabled) {
+                alert('Cannot record voice. Possible reasons are:\n\n1. Your device has no microphone.\n2. Your browser does not support the SpeechRecognition API.\n3. You\'ve refused the permission pop-up.');
+                return;
+            }
             this.recognition.start();
             this.recording = true;
         },
